c65ccd4fb5116c8ae8a1d9b30b2ee4dbb5f04c1c
mdwgti16 <mdwgti16@gmail.com>
2024-07-15 15:36:53 +0900

    _11004_K번째_수

---
+++ src/langjava/acmicpc/_11004_K번째_수.java
@@ -0,0 +1,78 @@
+package langjava.acmicpc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _11004_K번째_수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        quickSort(0, N - 1, A, K - 1);
+        System.out.println(A[K - 1]);
+    }
+
+    private static void quickSort(int s, int e, int[] A, int K) {
+        if (s >= e) {
+            return;
+        }
+
+        int p = partition(s, e, A);
+
+        if (K < p) {
+            quickSort(s, p - 1, A, K);
+        } else if (p < K) {
+            quickSort(p + 1, e, A, K);
+        }
+    }
+
+    private static int partition(int s, int e, int[] A) {
+        if (s + 1 == e) {
+            if (A[s] > A[e]) {
+                swap(A, s, e);
+            }
+            return e;
+        }
+
+        int M = (s + e) / 2;
+        swap(A, s, M);
+        int pivot = A[s];
+        int i = s + 1;
+        int j = e;
+
+        while (i <= j) {
+            while (A[j] > pivot) {
+                j--;
+            }
+
+            while (A[i] < pivot) {
+                i++;
+            }
+
+            if (i <= j) {
+                swap(A, i++, j--);
+            }
+        }
+
+        A[s] = A[j];
+        A[j] = pivot;
+
+        return j;
+    }
+
+    public static void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+}
