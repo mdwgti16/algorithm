8a3b92a5af0500f448a10f8a2037ca8eb9662c2a
mdwgti16 <mdwgti16@gmail.com>
2024-07-30 15:46:33 +0900

    _1929_소수_구하기

---
+++ src/langjava/acmicpc/_1929_소수_구하기_v2.java
@@ -0,0 +1,35 @@
+package langjava.acmicpc;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class _1929_소수_구하기_v2 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        boolean[] check = new boolean[M + 1];
+        int max = (int) Math.sqrt(M);
+        for (int i = 2; i <= max; i++) {
+            if (check[i]) {
+                continue;
+            }
+
+            for (int j = 2; i * j <= M; j++) {
+                check[i * j] = true;
+            }
+        }
+
+        for (int i = Math.max(2, N); i <= M; i++) {
+            if (!check[i]) {
+                bw.write(i + "\n");
+            }
+        }
+
+        bw.flush();
+    }
+}
